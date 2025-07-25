7b06095b173322e5841df4fe9c428a546df023aa
Hariharan <hariharen704@gmail.com>
2025-07-25 08:49:02 +0530

    Commit

--- server/server.js
+++ server/server.js
@@ -5,13 +5,15 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
 
 
 const app = express();
 const PORT = 3000;
 
-
 await connectDB()
+
 
 //Middleware
 app.use(express.json())
@@ -22,5 +24,8 @@ app.use(clerkMiddleware())
 //API Routes
 app.get('/' , (req , res) => res.send('Server is Live!'))
 app.use('/api/inngest' , serve({ client: inngest, functions }))
+app.use('/api/show' , showRouter)
+app.use('/api/booking' , bookingRouter)
+
 
 app.listen(PORT , () => console.log(`Server Listening at http://localhost:${PORT}`));
